b0be14fde933d059abf139b81a51f12ca3a95df4
Fahri Aydos <aydoscom@gmail.com>
2016-06-19 12:52:25 +0300

    closure externs added

---
+++ svgparser_externs.js
@@ -0,0 +1,12 @@
+// externs file for google's closure compiler
+// usage:
+// java -jar compiler.jar -O ADVANCED myapp.js --externs svgparser_externs.js > myapp.min.js
+var SVGParser = {
+	"parse" : function () {},
+	"scale" : function () {},
+	"move" : function () {},
+	"absolute" : function () {},
+	"path" : function () {},
+	"round" : function () {},
+	"table" : function () {},
+}
